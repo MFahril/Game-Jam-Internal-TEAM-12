eb16dbe682c7fbe1952880d1953c80c17e5026bb
M. Fahril Ilmi <m.fahrililmi99@gmail.com>
2024-10-05 08:17:28 +0700

    add core mechanic
    adding folders
    add player movement
    add player aiming
    add player shoot
    add Bullet prefabs

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+	public GameObject bulletPrefabs;
+	public Transform firePoint;
+	public float bulletForce = 20f;
+	public float fireRate = 0.3f;
+	private float fireTimeer;
+	
+	void Update()
+	{
+		if (Input.GetButton("Fire1") && fireTimeer <= 0f)
+		{
+			Shooting();
+			fireTimeer = fireRate;
+		}
+		else
+		{
+			fireTimeer -= Time.deltaTime;
+		}
+	}
+	private void Shooting()
+	{
+		GameObject bullet = Instantiate(bulletPrefabs, firePoint.position, firePoint.rotation);
+		Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+		rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+	}
+}
